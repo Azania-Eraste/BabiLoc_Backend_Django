9335377cc0cb0f49a51a34941a6fb19ba9fc123c
Azania-Eraste <kouadioazania@gmail.com>
2025-06-02 17:21:33 +0200

    Register urls

--- babiloc_backend/Auths/urls.py
+++ babiloc_backend/Auths/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.urls import path
+from .views import RegisterView, ActivateAccountView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,4 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate'),
+    
 ]
